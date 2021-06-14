0953caa207ff0d5ebcd1e565f0490859e0ac7012
Akshay Prasadan <akprasadan@gmail.com>
2021-06-14 17:56:44 -0400

    Created venv, requirements.txt, and setup.py file; finished much of Linear class

--- generalclassifier/main.py
+++ generalclassifier/main.py
@@ -1,33 +1,10 @@
 import numpy as np
 from numba import jit
-from preprocessing import train_test_split
-
-class SupervisedAlgorithm:
-
-    def __init__(self, features, output, split_proportion):
-        self.features = np.array(features)
-        self.output = np.array(output)
-        self.split_proportion = split_proportion
-        self.sample_size = self.features.shape[0]
-        self.train_size = np.ceil(self.sample_size * split_proportion)
-        # https://stackoverflow.com/a/14262743
-        self.train_rows = np.random.choice(self.train_size.shape[0], self.train_size, replace=False)
-        self.test_rows = np.setdiff1d(np.arange(self.sample_size), self.train_rows)
-        self.train_features = self.features[self.train_rows]
-        self.test_features = self.features[self.test_rows]
-        self.train_output = self.output[self.train_rows]
-        self.test_output = self.output[self.test_rows]
-    
-    def fit_to(self, input, output):
-        pass
-    
-    
+from preprocessing import train_test_split, scale_and_center
 
 
-class Classification(SupervisedAlgorithm):
-
+class Classification:
     def __init__(self, features, output, split_proportion, number_labels = None):
-        super().__init__(features, output, split_proportion)
         if number_labels is None: # Default procedure is to assume all labels appear in the output; otherwise, specify manually
             self.number_labels = len(np.unique(output)) 
         else: 
@@ -42,11 +19,13 @@ class Classification(SupervisedAlgorithm):
         self.test_features, \
         self.train_output, \
         self.test_output = train_test_split(features, output, split_proportion)
+    
+    def finalize_preprocess(self):
+        self.train_features = scale_and_center(self.train_features)
+        self.test_features = scale_and_center(self.test_features)
 
-class Regression(SupervisedAlgorithm):
-
-    def __init__(self, features, output, split_proportion, number_labels = None):
-        super().__init__(features, output, split_proportion)
+class Regression:
+    def __init__(self, features, output, split_proportion):
         self.sample_size, \
         self.train_size, \
         self.test_size, \
@@ -56,5 +35,9 @@ class Regression(SupervisedAlgorithm):
         self.test_features, \
         self.train_output, \
         self.test_output = train_test_split(features, output, split_proportion)
+
+    def finalize_preprocess(self):
+        self.train_features = scale_and_center(self.train_features)
+        self.test_features = scale_and_center(self.test_features)
 
     
