8b932c50241932a33d5e0df8dbb6fb6e16ec2ea1
Akshay Prasadan <akprasadan@gmail.com>
2021-06-17 16:01:33 -0400

    test jsmath

--- aklearn/poissonreg.py
+++ aklearn/poissonreg.py
@@ -159,8 +159,8 @@ class Poisson(Regression):
         ------
         In a Poisson model, we assume Y is Poisson, and that
         .. math:: 
-        
-            log E[Y|x] = beta^T x. 
+
+            \log \mathbb{E}[Y|x] = \beta^T x. 
 
         Here we return our estimate of :math: `E[Y|x]`.
         for a test data point x. This quantity reflects the mean 
